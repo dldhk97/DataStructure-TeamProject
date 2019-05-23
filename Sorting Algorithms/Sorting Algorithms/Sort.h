3e70778e7598cced020db7228d42cff65e856c5d
dldhk97 <dldhk97@gmail.com>
2019-05-23 12:59:39 +0900

    Type : Function Replace
    - utility 네임스페이스 삭제하고, utility::swap을 std::swap으로 대체하였음.

--- Sorting Algorithms/Sorting Algorithms/Sort.h
+++ Sorting Algorithms/Sorting Algorithms/Sort.h
@@ -3,14 +3,3 @@
 #include "BubbleSort.h"
 #include "InsertionSort.h"
 #include "QuickSort.h"
-
-namespace utility
-{
-	template <typename dataType>
-	void swap(dataType& data1, dataType& data2)
-	{
-		dataType temp = data1;
-		data1 = data2;
-		data2 = temp;
-	}
-}
