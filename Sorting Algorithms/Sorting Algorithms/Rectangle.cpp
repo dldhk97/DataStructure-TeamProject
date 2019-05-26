c077bf7b106190294196eefe49023ab8502c051a
dldhk97 <dldhk97@gmail.com>
2019-05-26 22:02:25 +0900

    Revert "dldhk97 branch - Feature Fix & Method Add"

--- Sorting Algorithms/Sorting Algorithms/Rectangle.cpp
+++ Sorting Algorithms/Sorting Algorithms/Rectangle.cpp
@@ -10,22 +10,17 @@ Rectangle::Rectangle(double width, double height)
 	this->height = height;
 }
 
-double Rectangle::getArea() const
-{
-	return this->width * this->height;
-}
-
 bool Rectangle::operator>(const Rectangle& T) const
 {
-	return getArea() > T.getArea() ? true : false;
+	return this->width * this->height > T.width * T.height ? true : false;
 }
 bool Rectangle::operator<(const Rectangle& T) const
 {
-	return this->getArea() < T.getArea() ? true : false;
+	return this->width* this->height < T.width* T.height ? true : false;
 }
 
 std::ostream& operator<<(std::ostream& os, const Rectangle& rec)
 {
-	os << "A:" << rec.getArea() << ", W:" << rec.width << ", H:" << rec.height;
+	os << "A:" << rec.width * rec.height << ", W:" << rec.width << ", H:" << rec.height;
 	return os;
 }
