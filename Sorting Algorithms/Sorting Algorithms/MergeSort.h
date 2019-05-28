fccab72b795b8c90c64c01ab202d1a6b272ff742
Administrator <Administrator@MFL-RAPTOR>
2019-05-29 00:00:43 +0900

    머지소트 논리커시브 완성

--- Sorting Algorithms/Sorting Algorithms/MergeSort.h
+++ Sorting Algorithms/Sorting Algorithms/MergeSort.h
@@ -37,4 +37,48 @@ namespace sorts
 		mergeSort(std::begin(arr), std::end(arr), std::begin(temp));
 		std::copy(std::begin(temp), std::end(temp), std::begin(arr));
 	}
+
+	template <typename dataType>
+	void nonRecursiveMergeSort(std::vector<dataType>& dataArr, const int capacity)
+	{
+		for (int n = 2; n / 4 < capacity / 2; n *= 2)
+		{
+			for (int i = 0; i < capacity; i += n)
+			{
+				if (n == 2)
+				{
+					if (i + 1 < capacity && dataArr[i + 1] < dataArr[i])
+						std::swap(dataArr[i], dataArr[i + 1]);
+				}
+				else
+				{
+					std::vector<dataType> tempArr;
+
+					int k = 0, l = i, m = i + n / 2;
+					while (m < capacity && l < i + n / 2 && m < i + n)
+					{
+						if (l < i + n / 2 && dataArr[l] < dataArr[m])
+							tempArr.push_back(dataArr[l++]);
+						else
+							tempArr.push_back(dataArr[m++]);
+					}
+					while (l < capacity && l < i + n / 2)
+					{
+						tempArr.push_back(dataArr[l++]);
+					}
+					while (m < capacity && m < i + n)
+					{
+						tempArr.push_back(dataArr[m++]);
+					}
+
+					for (int o = i; o < i + n; o++)
+					{
+						if (capacity - 1 < o) break;
+						dataArr[o] = tempArr[o - i];
+					}
+				}
+			}
+		}
+	}
+
 }
--- Sorting Algorithms/Sorting Algorithms/main.cpp
+++ Sorting Algorithms/Sorting Algorithms/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 #include "IOManager.h"
 #include "Sort.h"
@@ -13,78 +12,78 @@ int main()
 	std::vector<double> doubleArr;
 	std::vector<std::string> strArr;
 	std::vector<Rectangle> recArr;
-	int userInput;
+	
 	auto stoi = [](std::string input)->int {return std::stoi(input); };
 	auto stod = [](std::string input)->double {return std::stod(input); };
 
-	iom.FileReader<int>("C:\\Users\\Â÷¿¹½Â\\source\\repos\\RandomDataCreator\\int - 1000.txt", intArr, stoi);
-	iom.FileReader<double>("C:\\Users\\Â÷¿¹½Â\\source\\repos\\RandomDataCreator\\double - 1000.txt", doubleArr, stod);
-	iom.FileReader<std::string>("C:\\Users\\Â÷¿¹½Â\\source\\repos\\RandomDataCreator\\string - 1000.txt", strArr);
-	iom.FileReader<Rectangle>("C:\\Users\\Â÷¿¹½Â\\source\\repos\\RandomDataCreator\\rectangle - 1000.txt", recArr);
+	iom.FileReader<int>("C:\\Users\\Administrator\\source\\repos\\RandomDataCreator\\Debug\\int - 10.txt", intArr, stoi);
+	iom.FileReader<double>("C:\\Users\\Administrator\\source\\repos\\RandomDataCreator\\Debug\\double - 10.txt", doubleArr, stod);
+	iom.FileReader<std::string>("C:\\Users\\Administrator\\source\\repos\\RandomDataCreator\\Debug\\string - 10.txt", strArr);
+	iom.FileReader<Rectangle>("C:\\Users\\Administrator\\source\\repos\\RandomDataCreator\\Debug\\rectangle - 10.txt", recArr);
 
-	sorts::bubbleSort(intArr);
-	sorts::bubbleSort(doubleArr);
-	sorts::bubbleSort(strArr);
-	sorts::bubbleSort(recArr);
+	//sorts::bubbleSort(intArr);
+	//sorts::bubbleSort(doubleArr);
+	//sorts::bubbleSort(strArr);
+	//sorts::bubbleSort(recArr);
 
-	sorts::selectionSort(intArr);
-	sorts::selectionSort(doubleArr);
-	sorts::selectionSort(strArr);
-	sorts::selectionSort(recArr);
+	//sorts::selectionSort(intArr);
+	//sorts::selectionSort(doubleArr);
+	//sorts::selectionSort(strArr);
+	//sorts::selectionSort(recArr);
 
-	sorts::insertionSort(intArr);
-	sorts::insertionSort(doubleArr);
-	sorts::insertionSort(strArr);
-	sorts::insertionSort(recArr);
+	//sorts::insertionSort(intArr);
+	//sorts::insertionSort(doubleArr);
+	//sorts::insertionSort(strArr);
+	//sorts::insertionSort(recArr);
 
 	//sorts::insertionSort_list(intArr);
 	//sorts::insertionSort_list(doubleArr);
 	//sorts::insertionSort_list(strArr);
 	//sorts::insertionSort_list(recArr);
 
-	sorts::mergeSort(intArr);
-	sorts::mergeSort(doubleArr);
-	sorts::mergeSort(strArr);
-	sorts::mergeSort(recArr);
+	//sorts::mergeSort(intArr);
+	//sorts::mergeSort(doubleArr);
+	//sorts::mergeSort(strArr);
+	//sorts::mergeSort(recArr);
 
-	//sorts::mergeSort_non_recursive(intArr);
-	//sorts::mergeSort_non_recursive(doubleArr);
-	//sorts::mergeSort_non_recursive(strArr);
-	//sorts::mergeSort_non_recursive(recArr);
+	sorts::nonRecursiveMergeSort(intArr, intArr.size());
+	sorts::nonRecursiveMergeSort(doubleArr, doubleArr.size());
+	sorts::nonRecursiveMergeSort(strArr, strArr.size());
+	sorts::nonRecursiveMergeSort(recArr, recArr.size());
 
 	//sorts::mergeSort_natural(intArr);
 	//sorts::mergeSort_natural(doubleArr);
 	//sorts::mergeSort_natural(strArr);
 	//sorts::mergeSort_natural(recArr);
 
-	sorts::shellSort(intArr);
-	sorts::shellSort(doubleArr);
-	sorts::shellSort(strArr);
-	sorts::shellSort(recArr);
+	//sorts::shellSort(intArr);
+	//sorts::shellSort(doubleArr);
+	//sorts::shellSort(strArr);
+	//sorts::shellSort(recArr);
 
-	sorts::heapSort(intArr);
-	sorts::heapSort(doubleArr);
-	sorts::heapSort(strArr);
-	sorts::heapSort(recArr);
+	//sorts::heapSort(intArr);
+	//sorts::heapSort(doubleArr);
+	//sorts::heapSort(strArr);
+	//sorts::heapSort(recArr);
 
-	sorts::recursiveQuickSort(intArr, 0, intArr.size() - 1);
-	sorts::recursiveQuickSort(doubleArr, 0, doubleArr.size() - 1);
-	sorts::recursiveQuickSort(strArr, 0, strArr.size() - 1);
-	sorts::recursiveQuickSort(recArr, 0, recArr.size() - 1);
+	//sorts::recursiveQuickSort(intArr, 0, intArr.size() - 1);
+	//sorts::recursiveQuickSort(doubleArr, 0, doubleArr.size() - 1);
+	//sorts::recursiveQuickSort(strArr, 0, strArr.size() - 1);
+	//sorts::recursiveQuickSort(recArr, 0, recArr.size() - 1);
 
-	sorts::recursiveQuickSort_median(intArr, 0, intArr.size() - 1);
-	sorts::recursiveQuickSort_median(doubleArr, 0, doubleArr.size() - 1);
-	sorts::recursiveQuickSort_median(strArr, 0, strArr.size() - 1);
-	sorts::recursiveQuickSort_median(recArr, 0, recArr.size() - 1);
+	//sorts::recursiveQuickSort_median(intArr, 0, intArr.size() - 1);
+	//sorts::recursiveQuickSort_median(doubleArr, 0, doubleArr.size() - 1);
+	//sorts::recursiveQuickSort_median(strArr, 0, strArr.size() - 1);
+	//sorts::recursiveQuickSort_median(recArr, 0, recArr.size() - 1);
 
 	//sorts::recursiveQuickSort_non_recursive(intArr, 0, intArr.size() - 1);
 	//sorts::recursiveQuickSort_non_recursive(doubleArr, 0, doubleArr.size() - 1);
 	//sorts::recursiveQuickSort_non_recursive(strArr, 0, strArr.size() - 1);
 	//sorts::recursiveQuickSort_non_recursive(recArr, 0, recArr.size() - 1);
 
-	sorts::radixSort10(intArr);
-	sorts::radixSort16(intArr);
-	sorts::radixSortMasking(intArr);
+	//sorts::radixSort10(intArr);
+	//sorts::radixSort16(intArr);
+	//sorts::radixSortMasking(intArr);
 
 	return 0;
 }
