a82f0573192324b9aec0d770d1bd87d9b266fdb2
lunaoeu <lunaoeu@gmail.com>
2019-05-28 18:31:40 +0900

    Sort.h 수정

--- Sorting Algorithms/Sorting Algorithms/Sort.h
+++ Sorting Algorithms/Sorting Algorithms/Sort.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "SortManager.h"
 #include "BubbleSort.h"
-#include "InsertionSort.h"
-#include "QuickSort.h"
-#include "MergeSort.h"
 #include "SelectionSort.h"
-#include "ShellSort.h"
+#include "InsertionSort.h"
 #include "MergeSort.h"
+#include "ShellSort.h"
+#include "HeapSort.h"
+#include "QuickSort.h"
