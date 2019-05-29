b0598631f40fb70b71dd9c3c2c1f6dad9e6150ba
dldhk97 <dldhk97@gmail.com>
2019-05-29 23:08:43 +0900

    버블 정렬 속도 상승, 최적화. C스타일임. 출처 : https://ko.wikipedia.org/wiki/%EA%B1%B0%ED%92%88_%EC%A0%95%EB%A0%AC

--- Sorting Algorithms/Sorting Algorithms/BubbleSort.h
+++ Sorting Algorithms/Sorting Algorithms/BubbleSort.h
@@ -6,21 +6,21 @@ namespace sorts
 	template <typename dataType>
 	void bubbleSort(std::vector<dataType>& dataArr)
 	{
-		for (int i = 0; i < dataArr.size(); i++)
+		/*for (int i = 0; i < dataArr.size(); i++)
 		{
 			for (int j = 0; j < dataArr.size(); j++)
 			{
 				if (dataArr[i] < dataArr[j])
 					std::swap(dataArr[i], dataArr[j]);
 			}
-		}
-		/*for (auto i : dataArr)
-		{
-			for (auto j : dataArr)
-			{
-				if (i < j)
-					std::swap(i, j);
+		}*/
+		int size = dataArr.size();
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = 0; j < size - 1 - i; j++) {
+				if (dataArr[j] > dataArr[j + 1]) {
+					std::swap(dataArr[j], dataArr[j + 1]);
+				}
 			}
-		}*/	
+		}
 	}
 }
