7c2ba36e38833b4446c83c98719f27c13d361299
dldhk97 <dldhk97@gmail.com>
2019-05-29 23:09:19 +0900

    버블 정렬 코드 정리 괄호를 C스타일에서 C++ 스타일로 수정.

--- Sorting Algorithms/Sorting Algorithms/BubbleSort.h
+++ Sorting Algorithms/Sorting Algorithms/BubbleSort.h
@@ -6,18 +6,14 @@ namespace sorts
 	template <typename dataType>
 	void bubbleSort(std::vector<dataType>& dataArr)
 	{
-		/*for (int i = 0; i < dataArr.size(); i++)
-		{
-			for (int j = 0; j < dataArr.size(); j++)
-			{
-				if (dataArr[i] < dataArr[j])
-					std::swap(dataArr[i], dataArr[j]);
-			}
-		}*/
 		int size = dataArr.size();
-		for (int i = 0; i < size - 1; i++) {
-			for (int j = 0; j < size - 1 - i; j++) {
-				if (dataArr[j] > dataArr[j + 1]) {
+
+		for (int i = 0; i < size - 1; i++) 
+		{
+			for (int j = 0; j < size - 1 - i; j++) 
+			{
+				if (dataArr[j] > dataArr[j + 1]) 
+				{
 					std::swap(dataArr[j], dataArr[j + 1]);
 				}
 			}
