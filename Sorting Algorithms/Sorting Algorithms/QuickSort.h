aafdf50e9762b3efa79b33af0b6471c4ee9c081d
dldhk97 <dldhk97@gmail.com>
2019-05-31 17:21:39 +0900

    비재귀 퀵소트 리메이크. 새로운 코드 적용.

--- Sorting Algorithms/Sorting Algorithms/QuickSort.h
+++ Sorting Algorithms/Sorting Algorithms/QuickSort.h
@@ -144,6 +144,84 @@ namespace sorts
 	template <typename dataType>
 	void nonRecursiveQuickSort(std::vector<dataType>& dataArr)
 	{
+		// 퀵소트 함수
+		int start = 0;					//custom
+		int end = dataArr.size();		//custom
+
+		int i, j;
+		dataType pivot;
+		Stack<int> stack;
+
+		// 스택에 넣는다
+		//stack.arr[++stack.top] = end;
+		//stack.arr[++stack.top] = start;
+
+		stack.push(end);				//custom
+		stack.push(start);				//custom
+
+		// 스택이 없어질 때 까지 정렬을 계속한다
+		while (stack.isEmpty())
+		{
+			//start = stack.arr[stack.top--];
+			//end = stack.arr[stack.top--];
+
+			start = stack.pop();
+			end = stack.pop();
+
+			pivot = dataArr[0];
+			i = start;
+			j = end;
+			// 해당 부분 정렬 완료
+			if (end - start < 2)
+				continue;
+			// i가 j랑 교차할 때 까지 i하고 j값을 계속 바꾼다
+			while (i < j)
+			{
+				do
+				{
+					i++;
+				} while (dataArr[i] < pivot);
+				do
+				{
+					j--;
+				} while (dataArr[j] > pivot);
+				// i가 j보다 낮으면 i값하고 j값 교환
+				if (i < j)
+				{
+					//temp = arr[i];
+					//arr[i] = arr[j];
+					//arr[j] = temp;
+
+					std::swap(dataArr[i], dataArr[j]);		//custom
+				}
+			}
+			// i가 j를 교차하면 피봇값하고 j값 교환
+
+			//temp = arr[start];
+			//arr[start] = arr[j];
+			//arr[j] = temp;
+
+			std::swap(dataArr[start], dataArr[j]);			//custom
+
+			// j자리하고 바뀐 피봇값의 좌,우 정렬에 대한 값을 스택에 넣는다.
+
+			//stack.arr[++stack.top] = end;
+			//stack.arr[++stack.top] = j + 1;
+			//stack.arr[++stack.top] = j;
+			//stack.arr[++stack.top] = start;
+
+			stack.push(end);
+			stack.push(j + 1);
+			stack.push(j);
+			stack.push(start);
+		}
+	}
+
+
+
+	/*template <typename dataType>
+	void nonRecursiveQuickSort(std::vector<dataType>& dataArr)
+	{
 		const int size = dataArr.size();
 		Stack<int> stk;
 		dataType pivot;
@@ -195,7 +273,7 @@ namespace sorts
 				right = j - 1;
 			}
 		}
-	}
+	}*/
 
 	template <typename dataType>
 	void sort_of_three_key(std::vector<dataType> dataArr, int left, int middle, int right)
