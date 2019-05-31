6c9365ac4a1b52ee0b48a0c23b4db700f89e47bd
dldhk97 <dldhk97@gmail.com>
2019-05-31 17:28:04 +0900

    비 재귀 퀵 정렬 코드 정리

--- Sorting Algorithms/Sorting Algorithms/QuickSort.h
+++ Sorting Algorithms/Sorting Algorithms/QuickSort.h
@@ -153,8 +153,6 @@ namespace sorts
 		Stack<int> stack;
 
 		// 스택에 넣는다
-		//stack.arr[++stack.top] = end;
-		//stack.arr[++stack.top] = start;
 
 		stack.push(end);				//custom
 		stack.push(start);				//custom
@@ -162,18 +160,17 @@ namespace sorts
 		// 스택이 없어질 때 까지 정렬을 계속한다
 		while (stack.isEmpty())
 		{
-			//start = stack.arr[stack.top--];
-			//end = stack.arr[stack.top--];
-
 			start = stack.pop();
 			end = stack.pop();
 
 			pivot = dataArr[0];
 			i = start;
 			j = end;
+
 			// 해당 부분 정렬 완료
 			if (end - start < 2)
 				continue;
+
 			// i가 j랑 교차할 때 까지 i하고 j값을 계속 바꾼다
 			while (i < j)
 			{
@@ -188,92 +185,20 @@ namespace sorts
 				// i가 j보다 낮으면 i값하고 j값 교환
 				if (i < j)
 				{
-					//temp = arr[i];
-					//arr[i] = arr[j];
-					//arr[j] = temp;
-
 					std::swap(dataArr[i], dataArr[j]);		//custom
 				}
 			}
 			// i가 j를 교차하면 피봇값하고 j값 교환
 
-			//temp = arr[start];
-			//arr[start] = arr[j];
-			//arr[j] = temp;
-
 			std::swap(dataArr[start], dataArr[j]);			//custom
 
 			// j자리하고 바뀐 피봇값의 좌,우 정렬에 대한 값을 스택에 넣는다.
-
-			//stack.arr[++stack.top] = end;
-			//stack.arr[++stack.top] = j + 1;
-			//stack.arr[++stack.top] = j;
-			//stack.arr[++stack.top] = start;
-
 			stack.push(end);
 			stack.push(j + 1);
 			stack.push(j);
 			stack.push(start);
 		}
 	}
-
-
-
-	/*template <typename dataType>
-	void nonRecursiveQuickSort(std::vector<dataType>& dataArr)
-	{
-		const int size = dataArr.size();
-		Stack<int> stk;
-		dataType pivot;
-		int left, right, median;
-		int i, j;
-
-		left = 0;
-		right = size - 1;
-		median = left + (right - left) / 2;
-
-		stk.push(right); stk.push(left);
-		while (!stk.isEmpty())
-		{
-			left = stk.peek();
-			stk.pop();
-			right = stk.peek();
-			stk.pop();
-
-			while (left < right)
-			{
-				median = left + (right - left) / 2;
-
-				sorts::sort_of_three_key(dataArr, left, median, right);
-				pivot = dataArr[median];
-				dataArr[median] = dataArr[left];
-				dataArr[left] = pivot;
-
-				i = left;
-				j = right + 1;
-
-				while (true)
-				{
-					while (dataArr[++i] < pivot && i < dataArr.size() - 1);
-					while (pivot < dataArr[--j]);
-
-					if (j <= i)
-						break;
-					std::swap(dataArr[i], dataArr[j]);
-				}
-
-				dataArr[left] = dataArr[j];
-				dataArr[j] = pivot;
-
-				if (i < right)
-				{
-					stk.push(right);
-					stk.push(i);
-				}
-				right = j - 1;
-			}
-		}
-	}*/
 
 	template <typename dataType>
 	void sort_of_three_key(std::vector<dataType> dataArr, int left, int middle, int right)
