f83b85dc0eeb9c7d1ea73918503a7894c1b96eaf
sm <lunaoeu@gmail.com>
2019-06-01 14:43:16 +0900

    설명 주석 추가

--- Sorting Algorithms/Sorting Algorithms/InsertionSort.h
+++ Sorting Algorithms/Sorting Algorithms/InsertionSort.h
@@ -50,6 +50,11 @@ namespace sorts
 		auto first = std::begin(container);
 		auto last = std::end(container);
 
+		// std::upper_bound : 지정한 iterator를 순회하면서 *it값을 넘어가는 지점을 이진 탐색함. 그리고 그 위치(iterator) 반환. stable함
+		// std::rotate : 원소들을 (왼쪽으로) shift 하고 왼쪽 넘어간 애들은 오른쪽 끝으로 넣음
+
+		// 처음~처음 부터 처음~끝 까지 루프를 돈다
+		// 처음~it 안 루프에서 *it보다 큰 값 찾으면 그 위치 U부터 it까지의 값을 U부터 U+1 범위 내에서 rotate 
 		for (auto it = first; it != last; ++it)
 			std::rotate(std::upper_bound(first, it, *it), it, std::next(it));
 	}
